3ae74ee5474ad07e134d691bd1b583799101ef88
Foo <lcbc.lucascastro@gmail.com>
2015-07-17 20:41:22 -0300

    Aprimorado: Verifica itens repetidos

--- criadatabase.c
+++ criadatabase.c
@@ -1,12 +1,14 @@
 /* Author: ETA Team *
- * Last Modification: 07/16/2015 by Foo */
+ * Last Modification: 07/17/2015 by Foo */
 
+/* Pot menor, pot maior, espada de madeira, trapo velho, espada cega */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define QTS_TINHA_ANTES 2
-#define NUM_ITENS_NOVO 2
+#define QTS_TINHA_ANTES 5
+#define NUM_ITENS_NOVO 5
 
 typedef struct {
     /* Tipos:
@@ -19,27 +21,49 @@ typedef struct {
 
 int main (){
 
-	int i, j;
+	int i, j, flag;
 	char recebe;
 	FILE *database;
 	Item novos[NUM_ITENS_NOVO];
 
+	database = fopen("database.bin", "r+b");
+
+
+	if(database == NULL){
+		printf("macumba\n");
+		return 0;
+	}
+
+	fread(novos, QTS_TINHA_ANTES * sizeof(Item), 1, database);
+
 	for(i = QTS_TINHA_ANTES; i < NUM_ITENS_NOVO; i++){
+		flag = 1;
 		printf("Nome: \n");
 		scanf(" ");
 		fgets(novos[i].nome, 51, stdin);
 
-		printf("Tipo: \n");
-		scanf("%d", &novos[i].tipo);
-
-		printf("Valor: \n");
-		scanf("%d", &novos[i].valor);
-
-		novos[i].indice = i;
-
 		for(j = 0; novos[i].nome[j]; j++)
 			if(novos[i].nome[j] == '\n')
 				novos[i].nome[j] = '\0';
+
+		for(j = 0; (j < i) && (flag); j++)
+			if(strcmp(novos[i].nome, novos[j].nome) == 0)
+				flag = 0;
+
+		if(flag){
+			printf("Tipo: \n");
+			scanf("%d", &novos[i].tipo);
+
+			printf("Valor: \n");
+			scanf("%d", &novos[i].valor);
+
+			novos[i].indice = i;
+		}	
+
+		else{
+			printf("Item ja contido na database, insira outro\n");
+			i--;
+		}	
 	}
 
 	printf("\n\n\n");
@@ -54,14 +78,9 @@ int main (){
 	if(recebe != 'y')
 		return 0;
 
-	database = fopen("database.bin", "r+b");
-
-	if(database == NULL)
-		return 0;
-
 	fseek(database, 0, SEEK_END);
 
-	for(i = 0; i < NUM_ITENS_NOVO; i++)
+	for(i = QTS_TINHA_ANTES; i < NUM_ITENS_NOVO; i++)
 		if(!fwrite(&novos[i], sizeof(Item), 1, database)){
 			printf("erro no item %d!\n", i + 1);
 			return 0;
