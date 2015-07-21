f523c6a6d29c383552f6003e720c4993234a9cae
Foo <lcbc.lucascastro@gmail.com>
2015-07-21 20:30:04 -0300

    Programa auxiliar para ver os mapas na database, basta compila-lo

---
+++ vedatabase2.c
@@ -0,0 +1,63 @@
+/* Author: ETA Team *
+ * Last Modification: 07/20/2015 by Foo */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Define o struct do mapa */
+typedef struct {
+
+	int wall, player, used, enemyIndice, itemIndice;
+
+} Map;
+
+typedef struct{
+
+	int nivel, inimigos, tamI, tamJ;
+	Map mapa[100][100]; 
+} Nivel;
+
+int main (){
+
+	int i, j;
+	Nivel niveis;
+	FILE *arq;
+
+	arq = fopen("database2.bin", "r");
+
+	if(arq == NULL)
+		return 0;
+
+	while(fread(&niveis, sizeof(Nivel), 1, arq)){
+
+
+		printf("\n\nMapa de nivel: %d\nNumero de inimigos no mapa: %d\n\n\n", niveis.nivel, niveis.inimigos);
+
+		for(i = 0; i < niveis.tamI; i++){
+			for(j = 0; j < niveis.tamJ; j++){
+				if(niveis.mapa[i][j].player)
+					printf("P ");
+
+				else if(niveis.mapa[i][j].wall)
+					printf("X ");
+
+				else if(niveis.mapa[i][j].enemyIndice >= 0)
+					printf("E ");
+
+				else if(niveis.mapa[i][j].itemIndice >= 0)
+					printf("I ");
+
+				else
+					printf("  ");
+			}
+
+			printf("\n");
+		}
+	}
+
+	fclose(arq);
+
+	return 0;
+
+}
