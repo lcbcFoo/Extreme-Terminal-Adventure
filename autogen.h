7fddc0aebb6d7319c7472ce49095e428efbe7e02
Foo <lcbc.lucascastro@gmail.com>
2016-03-05 20:04:07 -0300

    Header das funcoes que geram um mapa aleatorio

---
+++ autogen.h
@@ -0,0 +1,23 @@
+/* Author: ETA Team *
+ * Last Modification: 03/05/2015 by Foo*/
+
+
+#ifndef AUTOGEN_H_INCLUDED
+#define AUTOGEN_H_INCLUDED 
+
+/* Gera um novo nivel */
+Nivel genNivel(int level, Player* player);
+
+/* Gera alguns elementos aleatorios para determinar o numero e tamanho de subsalas */
+void getRoomsStats(Nivel* nivel, int *maxSize, int* rooms);
+
+/* Abre uma subsala completamente fechada */
+void fixRoom(Nivel* nivel);
+
+/* Testa se o mapa esta totalmente interligado */
+void testRoom(Nivel* nivel);
+
+/* Gera um mapa */
+void genRoom(Nivel* nivel);
+
+#endif /* AUTOGEN_H_INCLUDED */
